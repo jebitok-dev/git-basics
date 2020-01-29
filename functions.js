8a8abdaa3b2c6c112c20c0cb11c6d9d4255137a5
jebitok-dev <sharonkosgei4@gmail.com>
2020-01-29 18:24:21 +0300

    referencing a value

--- functions.js
+++ functions.js
@@ -6,12 +6,13 @@ function div(a,b) {
 
 }
 
-Let dividend = div (6,2);
+let dividend = div (6,2);
 console.log(dividend);
 
 // Function expression
-let div_exp = function(a,b){
-return a,b;
+let div_exp = function(a,b)
+{
+return a / b;
 };
 
 //let dividend_exp=div_exp (7,2);
@@ -19,6 +20,15 @@ console.log(dividend_exp)
 
 // Arrow Function
 let dividend_exp = (a,b) => {
-return a%b;
+return a % b
+}
 
-let dividend_exp=
+let person = { 
+    name: " Sharon Jebitok",
+    gender: " female ",
+    email: " sharonkosgei4@gmail.com ",
+    occupation: " student software development "
+    };
+    //Reference a value from key value pair in person object
+    let jina=person.name;
+    console.log('My name is ' + jina + ' and I am a '+ person.occupation);
