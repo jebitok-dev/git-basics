b41530fd575c6bb66c67fae6147522dd13e7d041
jebitok-dev <sharonkosgei4@gmail.com>
2020-01-29 14:39:13 +0300

    initialize js file

---
+++ functions.js
@@ -0,0 +1,24 @@
+// Arithmetic operations
+
+// Function declaration
+function div(a,b) {
+    return a/b;
+
+}
+
+Let dividend = div (6,2);
+console.log(dividend);
+
+// Function expression
+let div_exp = function(a,b){
+return a,b;
+};
+
+//let dividend_exp=div_exp (7,2);
+console.log(dividend_exp)
+
+// Arrow Function
+let dividend_exp = (a,b) => {
+return a%b;
+
+let dividend_exp=
